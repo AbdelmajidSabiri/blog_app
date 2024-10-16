b68f521ebc9f9e5c9203bc39363a32cb223953ea
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-16 18:15:54 +0100

    Add routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,47 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Router,
+} from "react-router-dom";
+import Register from "./pages/Register"
+import Single from "./pages/Single"
+import Home from "./pages/Home"
+import Write from "./pages/Write"
+import Login from "./pages/Login"
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/write",
+    element: <Write/>,
+  },
+  
+  {
+  path: "/login",
+  element: <Login/>,
+  },
+]);
 
 function App() {
   return (
     <div>
-      Hello woooorld!
+      <RouterProvider router = {router}/>
     </div>
   );
 }
