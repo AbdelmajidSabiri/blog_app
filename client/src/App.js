f0e3b87c3510c8e584ebb73689503791f8d0ec46
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-17 20:46:47 +0100

    Modify routes so all pages have footer and header by adding layout method

--- client/src/App.js
+++ client/src/App.js
@@ -2,40 +2,59 @@ import {
   createBrowserRouter,
   RouterProvider,
   Router,
+  Outlet,
 } from "react-router-dom";
 import Register from "./pages/Register"
 import Single from "./pages/Single"
 import Home from "./pages/Home"
 import Write from "./pages/Write"
 import Login from "./pages/Login"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
-
-
+const Layout = () => {
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
+    element: <Layout/>,
+    children : [
+      {
+        path: "/",
+        element: <Home/>
+      },
+
+      {
+        path: "/register",
+        element: <Register/>
+      },
+    
+      {
+        path: "/single",
+        element: <Single/>
+      },
+      
+      {
+        path: "/write",
+        element: <Write/>
+      },
+    
+      {
+      path: "/login",
+      element: <Login/>
+      },
+    ]
   },
 
-  {
-    path: "/single",
-    element: <Single/>,
-  },
-  {
-    path: "/write",
-    element: <Write/>,
-  },
-
-  {
-  path: "/login",
-  element: <Login/>,
-  },
 ]);
 
 
@@ -46,5 +65,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
