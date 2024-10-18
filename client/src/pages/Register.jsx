423fe559adcf0480c8a6020b46cb1eeb41265b37
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-18 10:42:40 +0100

    Create form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () =>{
     return (
-        <div>Register</div>
+        <div className="auth">
+            <h1>Register</h1>
+            <form action="">
+                <input required type="text" placeholder="username"/>
+                <input required type="email" placeholder="email"/>
+                <input required type="password" placeholder="password"/>
+
+                <button>Login</button>
+                <p>this is error</p>
+                <span>Do you have an account? <Link to="/login">Login</Link>
+                </span>
+            </form>
+        </div>
     )
 }
 
