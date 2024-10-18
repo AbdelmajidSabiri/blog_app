a8a867960017c3eea1a269c42d5c5aa8f5b23606
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-18 10:32:30 +0100

    Create login page form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Login = () =>{
     return (
-        <div>Login</div>
+        <div className="auth">
+            <h1>Login</h1>
+            <form action="">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password"/>
+                <button>Login</button>
+                <p>this is error</p>
+                <span>Don't you have an account? <Link to="/register">Register</Link>
+                </span>
+            </form>
+        </div>
     )
 }
 
